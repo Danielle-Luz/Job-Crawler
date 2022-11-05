ca184ac488a2466de6606bc33e909efc8bcee1b4
Axionauta <daniln2209@gmail.com>
2022-11-05 12:59:35 -0300

    feat: adiciona anotações básicas

--- src/api/Api.java
+++ src/api/Api.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping(value = "/trabalhos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class Api {
 
 }

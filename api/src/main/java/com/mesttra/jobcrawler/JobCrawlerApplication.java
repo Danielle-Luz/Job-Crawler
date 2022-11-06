be48750c557045b23d7e246f4b7f4572f52a7ed3
Axionauta <daniln2209@gmail.com>
2022-11-06 04:04:56 -0300

    feat: insere arquivos da api em pasta

--- api/src/main/java/com/mesttra/jobcrawler/JobCrawlerApplication.java
+++ api/src/main/java/com/mesttra/jobcrawler/JobCrawlerApplication.java
@@ -2,6 +2,9 @@ package com.mesttra.jobcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JobCrawlerApplication {
@@ -10,4 +13,13 @@ public class JobCrawlerApplication {
 		SpringApplication.run(JobCrawlerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/trabalhos").allowedOrigins("*");
+			}
+		};
+	}
 }

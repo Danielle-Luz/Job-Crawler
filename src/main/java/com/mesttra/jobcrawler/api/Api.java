3b9c61a9fd408714897367ec3e0abd016fe86e5b
Axionauta <daniln2209@gmail.com>
2022-11-05 23:10:16 -0300

    feat: atualiza arquivos da api

--- src/main/java/com/mesttra/jobcrawler/api/Api.java
+++ src/main/java/com/mesttra/jobcrawler/api/Api.java
@@ -1,14 +1,17 @@
-package api;
+package com.mesttra.jobcrawler.api;
 
-import control.Crawler;
+import com.mesttra.jobcrawler.control.*;
+import com.mesttra.jobcrawler.model.Job;
 
-import org.jsoup.select.Elements;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @RequestMapping(value = "/trabalhos", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -16,8 +19,10 @@ public class Api {
 
     @PostMapping
     @ResponseBody
-    public Elements getJobsFromUrl (@RequestBody String url) {
+    @CrossOrigin( origins = "http://localhost:8080")
+    public List<Job> getJobsFromUrl (@RequestBody String url) {
         Crawler crawler = new Crawler(url);
         return crawler.getPageDevJobs();
     }
 }
+

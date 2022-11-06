4ffdf30b580b994c28b7923c2239aeaa31bd21de
Axionauta <daniln2209@gmail.com>
2022-11-06 02:53:22 -0300

    feat: adiciona arquivos

--- src/main/java/com/mesttra/jobcrawler/api/Api.java
+++ src/main/java/com/mesttra/jobcrawler/api/Api.java
@@ -5,17 +5,12 @@ import com.mesttra.jobcrawler.model.Job;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @RequestMapping(value = "/trabalhos", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,10 +18,7 @@ public class Api {
 
     @PostMapping
     @ResponseBody
-    @CrossOrigin( origins = "http://localhost:8080")
-    public List<Job> getJobsFromUrl (@RequestBody String url, HttpServletRequest request, HttpServletResponse response) {
-    	response.setHeader("Access-Control-Allow-Origin", request.getHeader(HttpHeaders.ORIGIN));
-    	
+    public List<Job> getJobsFromUrl (@RequestBody String url) {
         Crawler crawler = new Crawler(url);
         return crawler.getPageDevJobs();
     }
